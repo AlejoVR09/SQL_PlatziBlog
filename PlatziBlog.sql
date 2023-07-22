a61097a2f82b3270f0ff21d4736684c38cfc2023
Alejo <Alejandrovanegas09@outlook.com>
2023-07-21 20:05:17 -0500

    Advanced queries with group by, order by, having, and dinamic fields

--- PlatziBlog.sql
+++ PlatziBlog.sql
@@ -183,13 +183,171 @@ INSERT INTO etiquetas (nombre_etiqueta) VALUES
   ('Concierto'),
   ('Cocina');
 
+-- Dummy data of posts
+INSERT INTO posts (titulo, fecha_publicacion, contenido, estatus, usuario_id, categoria_id) VALUES
+  ('Nuevas tendencias en tecnología', '2023-05-15 08:30:00', 'Hoy hablaremos sobre las últimas tendencias tecnológicas...', 'activo', 1, 1),
+  ('Consejos para una vida saludable', '2023-05-16 12:15:00', 'En este post, compartiremos algunos consejos para llevar una vida más saludable...', 'activo', 2, 6),
+  ('Las mejores ofertas de verano', '2023-05-17 16:45:00', 'No te pierdas estas increíbles ofertas de verano...', 'activo', 3, 2),
+  ('Recetas veganas deliciosas', '2023-05-18 10:00:00', 'Descubre estas deliciosas recetas veganas para toda la familia...', 'activo', 4, 5),
+  ('Los destinos más asombrosos para viajar', '2023-05-19 14:20:00', 'Estos destinos te dejarán sin aliento...', 'activo', 5, 7),
+  ('Consejos para mejorar tu rendimiento deportivo', '2023-05-20 11:30:00', 'Si te apasiona el deporte, no te pierdas estos consejos para mejorar tu rendimiento...', 'activo', 6, 4),
+  ('Ideas para decorar tu hogar', '2023-05-21 09:00:00', 'Transforma tu hogar con estas fantásticas ideas de decoración...', 'activo', 7, 3),
+  ('Los mejores eventos de entretenimiento', '2023-05-22 19:30:00', 'Descubre los eventos más emocionantes y divertidos de la ciudad...', 'activo', 8, 8),
+  ('Aprende a cocinar como un chef', '2023-05-23 15:10:00', 'En este post, aprenderás algunos trucos de cocina para impresionar a tus invitados...', 'activo', 9, 10),
+  ('Consejos financieros para el futuro', '2023-05-24 13:00:00', 'Planifica tu futuro financiero con estos consejos prácticos...', 'activo', 10, 2),
+  ('Consejos financieros para el futuro', '2023-05-24 13:00:00', 'Planifica tu futuro financiero con estos consejos prácticos...', 'inactivo', 10, 2),
+('Nuevas tendencias en tecnología', '2022-03-15 08:30:00', 'Hoy hablaremos sobre las últimas tendencias tecnológicas...', 'activo', 1, 1),
+  ('Consejos para una vida saludable', '2023-05-16 12:15:00', 'En este post, compartiremos algunos consejos para llevar una vida más saludable...', 'activo', 2, 6),
+  ('Las mejores ofertas de verano', '2021-07-10 16:45:00', 'No te pierdas estas increíbles ofertas de verano...', 'activo', 3, 2),
+  ('Recetas veganas deliciosas', '2022-11-20 10:00:00', 'Descubre estas deliciosas recetas veganas para toda la familia...', 'activo', 4, 5),
+  ('Los destinos más asombrosos para viajar', '2023-09-05 14:20:00', 'Estos destinos te dejarán sin aliento...', 'activo', 5, 7),
+  ('Consejos para mejorar tu rendimiento deportivo', '2021-06-30 11:30:00', 'Si te apasiona el deporte, no te pierdas estos consejos para mejorar tu rendimiento...', 'activo', 6, 4),
+  ('Ideas para decorar tu hogar', '2023-04-18 09:00:00', 'Transforma tu hogar con estas fantásticas ideas de decoración...', 'activo', 7, 3),
+  ('Los mejores eventos de entretenimiento', '2022-08-22 19:30:00', 'Descubre los eventos más emocionantes y divertidos de la ciudad...', 'activo', 8, 8),
+  ('Aprende a cocinar como un chef', '2021-12-12 15:10:00', 'En este post, aprenderás algunos trucos de cocina para impresionar a tus invitados...', 'activo', 9, 10),
+  ('Consejos financieros para el futuro', '2023-01-25 13:00:00', 'Planifica tu futuro financiero con estos consejos prácticos...', 'activo', 10, 2);
+-- Dummy data of etiquetas
+INSERT INTO etiquetas (nombre_etiqueta) VALUES
+  ('Tecnología'),
+  ('Moda'),
+  ('Hogar'),
+  ('Deportes'),
+  ('Alimentación'),
+  ('Salud'),
+  ('Viajes'),
+  ('Entretenimiento'),
+  ('Educación'),
+  ('Automóviles');
 
+-- Dummy data of this trannsitive table
+INSERT INTO posts_etiquetas (post_id, etiqueta_id) VALUES
+  (1, 1),
+  (1, 2),
+  (2, 6),
+  (3, 3),
+  (3, 8),
+  (4, 5),
+  (5, 7),
+  (5, 9),
+  (6, 4),
+  (7, 3);
+
+-- Dummy data of comentarios
+INSERT INTO comentarios (cuerpo_comentario, usuario_id, post_id) VALUES
+  ('Excelente artículo, gracias por compartir esta información.', 1, 1),
+  ('Estoy de acuerdo contigo, el tema de la salud es fundamental.', 2, 2),
+  ('Las ofertas son increíbles, no puedo esperar para ir de compras.', 3, 3),
+  ('Gracias por las recetas, voy a probarlas este fin de semana.', 4, 4),
+  ('Los destinos son impresionantes, ya quiero planificar mi próximo viaje.', 5, 5),
+  ('Estos consejos me han ayudado a mejorar mi rendimiento en el deporte.', 6, 6),
+  ('Me encanta cómo decoraste tu hogar, ¡se ve hermoso!', 7, 7),
+  ('El evento de entretenimiento fue increíble, ¡gracias por el dato!', 8, 8),
+  ('He preparado la receta y a todos les encantó, ¡gracias por compartir!', 9, 9),
+  ('Estos consejos financieros me han sido de gran ayuda, ¡gracias!', 10, 10);
 
 SELECT * FROM usuarios;
+SELECT * FROM posts;
+SELECT * FROM comentarios;
+SELECT * FROM etiquetas;
+SELECT * FROM categorias;
+
+-- GROUP BY Queries
+-- show how many posts are 'activo' or 'inactivo'
+SELECT estatus, COUNT(*) AS post_number
+FROM posts
+GROUP BY estatus;
+
+-- show how many posts were created in current and past years
+SELECT	YEAR(fecha_publicacion) AS post_year, COUNT(*) AS post_number
+FROM posts
+GROUP BY post_year;
+
+-- show how many posts are created per month
+SELECT	MONTHNAME(fecha_publicacion) AS post_month, COUNT(*) AS post_number
+FROM posts
+GROUP BY post_month;
+
+-- combine the status and monthname for showing how many per month are 'activo' or 'inactivo' 
+SELECT	estatus, MONTHNAME(fecha_publicacion) AS post_date, COUNT(*) AS post_number
+FROM	posts
+GROUP BY estatus, post_date;
+
+-- ORDER BY Queries
+
+-- It shows the posts order by the date they were created
+SELECT	*
+FROM posts
+ORDER BY fecha_publicacion ASC;
+
+-- shows the same than above but in descendent way
+SELECT	*
+FROM posts
+ORDER BY fecha_publicacion DESC;
+
+-- order the posts by the tittle
+SELECT	*
+FROM posts
+ORDER BY titulo ASC;
+
+-- the same than above but descendant
+SELECT	*
+FROM posts
+ORDER BY titulo DESC;
+
+-- it limits the records shown in 5
+SELECT	*
+FROM posts
+ORDER BY usuario_id ASC
+LIMIT 5;
+
+-- this query group the records by the status and month they were created, and order them by the month
+SELECT	MONTHNAME(fecha_publicacion) AS post_month, estatus, COUNT(*) AS post_quantity
+FROM posts
+GROUP BY estatus, post_month
+ORDER BY post_month;
+
+-- HAVING Queries
+
+-- having is useful for dinamic fields like the one that use MONTHNAME or COUNT
+
+-- this one is and example that shows an error
+SELECT	MONTHNAME(fecha_publicacion) AS post_month, estatus, COUNT(*) AS post_quantity
+FROM posts
+WHERE post_quantity > 1
+GROUP BY estatus, post_month
+ORDER BY post_month;
+
+-- it shows the months that have more than 1 post created
+SELECT	MONTHNAME(fecha_publicacion) AS post_month, estatus, COUNT(*) AS post_quantity
+FROM posts
+GROUP BY estatus, post_month
+HAVING post_quantity > 1
+ORDER BY post_month;
+
+SELECT new_table_projection.date, COUNT(*) AS posts_count
+FROM (
+    SELECT DATE(MIN(fecha_publicacion)) AS date, YEAR(fecha_publicacion) AS post_year
+    FROM posts
+    GROUP BY post_year
+) AS new_table_projection
+GROUP BY new_table_projection.date 
+ORDER BY new_table_projection.date;
+
+SELECT *
+FROM posts
+WHERE fecha_publicacion = (
+	SELECT MAX(fecha_publicacion)
+	FROM posts
+);
+
+SELECT MAX(fecha_publicacion)
+FROM posts;
+
 
 DROP TABLE usuarios;
 DROP TABLE etiquetas;
 DROP TABLE categorias;
+DROP TABLE posts;
 
 
 /*
